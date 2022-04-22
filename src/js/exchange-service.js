4835ffc002a99e18605b06d67fa02535ee4e1b8d
Sploob <crayiphone@gmail.com>
2022-04-22 12:01:59 -0700

    WIP: error handling for bad requests

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,12 +1,14 @@
 export default class ExchangeApi {
   static async getRates(currency) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}?`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/?`);
       if (!response.ok) {
-        throw Error(response.statusText);
+        console.log(response.statusText);
+        // throw Error(response.statusText);
       }
       return response.json();
     } catch(error) {
+      console.log(error.message);
       return error.message;
     }
   }
