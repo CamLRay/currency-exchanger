6593b3c2b1943875f53d0020063ecfbde1c3af68
Sploob <crayiphone@gmail.com>
2022-04-22 10:04:49 -0700

    add api call

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeApi from './js/exchange-service';
+
+function convertCurrency (data, value, exchangeTo) {
+  const exchangeRate = data[conversion_rates][exchangeTo];
+  return value * exchangeRate;
+}
+
+async function exchangeApiCall(currency, value, exchangeTo) {
+  const data = await ExchangeApi.getRates(currency)
+  convertCurrency(data, value, exchangeTo)
+}
