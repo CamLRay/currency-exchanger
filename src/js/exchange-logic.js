dedd6b52616dcc4f183f5f06778cada9b8a388ac
Sploob <crayiphone@gmail.com>
2022-04-22 11:30:39 -0700

    add check for valid currency

--- src/js/exchange-logic.js
+++ src/js/exchange-logic.js
@@ -1,8 +1,11 @@
 import $ from 'jquery';
 
 export function convertCurrency (data, value, exchangeTo) {
-  const exchangeRate = parseFloat(data["conversion_rates"][exchangeTo]);
-  console.log(parseFloat(value) * exchangeRate);
-  let conversion = parseFloat(value) * exchangeRate;
-  $("h1").text(`${value} ${data["base_code"]} is ${conversion}${exchangeTo}`);
+  if(Object.keys(data["conversion_rates"]).includes(exchangeTo)){
+    const exchangeRate = parseFloat(data["conversion_rates"][exchangeTo]);
+    let conversion = parseFloat(value) * exchangeRate;
+    $("h1").text(`${value} ${data["base_code"]} is ${conversion}${exchangeTo}`);
+  } else {
+    $("h1").text(`${exchangeTo} is not a valid currency or isn't in our database. Please try using ISO 4217 Three Letter Currency Code e.g. "EUR" for Euro.`);
+  }
 }
