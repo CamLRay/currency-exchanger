7787e60ceb39b695713e076876dc5ee365568edd
Sploob <crayiphone@gmail.com>
2022-04-22 18:22:49 -0700

    refactor error handling for bad fetch

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -3,12 +3,11 @@ export default class ExchangeApi {
   static async getRates(currency) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}?`);
-      if (!response.ok) {
-        const jsonResponse = await response.json();
-        $(".error-message").html(`<h1>Error: ${jsonResponse["error-type"]}</h1>`);
-        throw Error(response.statusText);
+      if (response.ok) {
+        return response.json();
+      } else {
+        $(".error-message").html(`<h1>Error: ${response.status} ${response.statusText}</h1>`);
       }
-      return response.json();
     } catch(error) {
       $(".error-message").html(`<h1>${error.message}</h1>`);
       return error.message;
